dc94bf8eed12936dbb5c43ad754510cf2928d37e
Raihan567 <aburaihansarker567@gmail.com>
2022-06-30 23:04:54 +0600

    complete gtp3 section

--- src/Components/Feature/Feature.jsx
+++ src/Components/Feature/Feature.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import "./Feature.css";
-const Feature = ({ text, title }) => {
+const Feature = ({ title, text }) => {
   return (
     <div className="gpt3__features-container__feature">
       <div className="gpt3__features-container__feature-title">
         <div>
-          <h1>{title}</h1>
+          <h1 >{title}</h1>
         </div>
       </div>
       <div className="gpt3__features-container__feature-text">
--- src/Containers/WhatGPT3/WhatGPT3.jsx
+++ src/Containers/WhatGPT3/WhatGPT3.jsx
@@ -3,20 +3,19 @@ import { Feature } from "../../Components";
 import "./WhatGPT3.css";
 const WhatGPT3 = () => {
   return (
-    <div className="gpt3__whatgpt3 section__margin" id="gpt3">
+    <div className="gpt3__whatgpt3 section__margin " id="whatgpt3">
       <div className="gpt3__whatgpt3-feature">
         <Feature
-          title="What is GPT-3?"
+          title="WhatIsGPT3"
           text="We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by."
-        />
+        ></Feature>
       </div>
       <div className="gpt3__whatgpt3-heading">
         <h1 className="gradient__text">
-          The possibilities are beyond your imagination
+          The possibilities are beyond <br /> your imagination
         </h1>
         <p>Explore The Library</p>
       </div>
-
       <div className="gpt3__whatgpt3-container">
         <Feature
           title="Chatbots"
